b9a01d2fab76cd6fa040efff1df02a8c4a835a24
dunck2020 <j.dunckel@hotmail.com>
2021-11-20 11:52:57 -0500

    Add project files.

---
+++ Models/DepartmentLocation.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace EmployeesCh12.Models
+{
+    public class DepartmentLocation
+    {
+        // Composite Primary key and foreign key for Department
+        public int DepartmentID { get; set; }
+
+        // Composite Primary key and foreign key for Location
+        public int LocationID { get; set; }
+
+        // Nav properties
+        public Department Department { get; set; }
+        public Location Location { get; set; }
+
+    }
+}
